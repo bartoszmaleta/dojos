394c98124ed41e8529874c29c1ff8b2adaf37141
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-16 10:47:18 +0200

    Added 100_doors exercise

---
+++ 100_doors.py
@@ -0,0 +1,17 @@
+# working
+
+doors = [0]*100
+for tries in range(1, 101):
+    for number_of_door in range(tries-1, 100, tries):
+        if doors[number_of_door] == 0:
+            doors[number_of_door] = 1
+        elif doors[number_of_door] == 1:
+            doors[number_of_door] = 0
+
+opendoors = []
+for i, value in enumerate(doors):
+    if value == 1:
+        opendoors.append(i+1)
+
+
+print('Opened doors: ', opendoors)
